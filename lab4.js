21e37a03b10f9dee44443ee61e831f7579ee4ece
HoàngKhangKhuPho4 <22130116@st.hcmuaf.edu.vn>
2023-12-23 21:13:58 +0700

    update code part6

--- lab4.js
+++ lab4.js
@@ -1,14 +1,27 @@
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
+  var currentTab = document.getElementById(tabName);
+
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
+    // Kiểm tra nếu tab hiện tại đã được mở, đóng nó và thoát khỏi hàm
+    if (tabcontent[i] === currentTab && currentTab.style.display === "block") {
+      currentTab.style.display = "none";
+      evt.currentTarget.className = evt.currentTarget.className.replace(
+        " active",
+        ""
+      );
+      return;
+    }
     tabcontent[i].style.display = "none";
   }
+
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(tabName).style.display = "block";
+
+  currentTab.style.display = "block";
   evt.currentTarget.className += " active";
 }
 
